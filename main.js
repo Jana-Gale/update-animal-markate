22f04fc32f915009dd49b0f01092cf6d316edb87
jana gale <najaajoon0@gmail.com>
2024-01-13 19:43:21 -0800

    my website xoolo

---
+++ main.js
@@ -0,0 +1,210 @@
+let cart_btn = document.getElementById("cart-btn")
+
+
+let cart_itames = document.getElementById("cart_itames")
+
+cart_btn.addEventListener("click", (e) => {
+    cart_itames.classList.toggle("hidden")
+
+
+})
+
+//Total_price all
+let Total_price = document.querySelector("#Total_price");
+console.log(Total_price.innerText);
+
+//counter_cart
+let counter_cart = document.getElementById("counter_cart")
+console.log(counter_cart);
+
+// create list to store the products
+let save_cart_items = []
+
+
+//Buy button
+let Buy_btns = document.querySelectorAll("#Buy_btn")
+
+//loop over the Buy_btns
+Buy_btns.forEach((Buy_btn) => {
+
+    //make event each button
+    Buy_btn.addEventListener("click", (e) => {
+        //each btn it's parentElement
+
+
+        //know if user wants to buy someting.
+        let message = confirm('Do you want to buy this item.')
+        console.log(message);
+
+
+
+        if (message) {
+
+
+            let parant_Elent = Buy_btn.parentElement;
+            //gran ParantElemt the DIV of each btn
+            let Big_parant_div = parant_Elent.parentElement;
+
+            //create object all items
+            let all_items = {
+                id: Big_parant_div.querySelector("#product_id").value,
+                image: Big_parant_div.querySelector("#image").getAttribute("src"),
+                product_name: Big_parant_div.querySelector("#product_name").innerText,
+                price: Big_parant_div.querySelector("#price").innerText.replace("$", ""),
+                quantity: 1
+            }
+
+            //do not add in the cart 2 same itemes
+            //ogoow sheygaan horay ma loogatay maya am haa
+            let isIntheCart = save_cart_items.filter(item => item.id === all_items.id).length > 0;
+            console.log(isIntheCart);
+
+            if (!isIntheCart) { //if length <<1
+
+
+
+                AddItemsTotheCart(all_items)
+
+
+
+            } else {
+                alert("You have already purchased this item.")
+                return
+            }
+
+            //sooqabo cart_item walbo oo div ah
+            let Each_cart_div = cart_itames.querySelectorAll("#cart_item");
+            console.log(Each_cart_div);
+
+            //mid mid usooqabo div cart
+            Each_cart_div.forEach(indivutual_div => {
+                console.log(indivutual_div);
+                //is item in the cart
+                if (indivutual_div.querySelector("#product_id").value === all_items.id) {
+
+                    console.log("yes are same");
+                    //badi qauntity gas
+                    IncreasItem(indivutual_div, all_items)
+                    decreaseItem(indivutual_div, all_items)
+                    RemoveItem(indivutual_div, all_items)
+                }
+            })
+
+            //add last to the arrays
+            save_cart_items.push(all_items)
+
+
+            //calculate all Total_price each item
+            Calculate_Total()
+            console.log(save_cart_items.length);
+
+
+        }
+
+    })
+
+})
+
+
+
+
+
+// AddItemsTotheCart function
+
+function AddItemsTotheCart(Itames) {
+    cart_itames.insertAdjacentHTML("afterbegin", `
+    
+
+    <div id="cart_item" class="flex items-center justify-around border-b-2 border-[#008080] pb-4 mb-4">
+    <input type="hidden" name="" id="product_id" value="${Itames.id}">
+    <img src="${Itames.image}" alt="" class="h-20 w-20">
+    <h4 class="text-xl">${Itames.product_name}</h4>
+    <a href="#" id="small_btn_minus" class="bg-[#008080] text-white py-2 px-4 rounded cursor-pointer">&minus;</a>
+    <h4   id="quantity" class="text-xl">${Itames.quantity}</h4>
+    <a href="#" id="small_btn_plus"  action="increase" class="cursor-pointer bg-[#008080] text-white py-2 px-4 rounded ">&plus;</a>
+    <span id="price" class="text-xl ">${Itames.price}</span>
+    <a href="#" id="small_btn_remove" class="bg-[#ff1212] text-white py-2 px-4 rounded  font-semibold">&times;</a>
+</div>
+    
+    `)
+
+}
+
+
+
+
+
+//calculation function
+
+function Calculate_Total() {
+    let Total = 0;
+    save_cart_items.forEach(item => {
+        Total += item.quantity * item.price;
+        console.log(Total);
+    })
+
+
+    Total_price.innerText = Total;
+    //How man itames i bought
+    counter_cart.innerText = save_cart_items.length;
+
+
+}
+
+
+
+// Increasment function
+
+function IncreasItem(indivutual_item, products_object) {
+    indivutual_item.querySelector("#small_btn_plus").addEventListener("click", (e) => {
+        console.log("alx it worked");
+        console.log(indivutual_item.querySelector("#quantity").innerText);
+
+        indivutual_item.querySelector("#quantity").innerText = ++products_object.quantity
+        Calculate_Total()
+    })
+
+
+}
+
+
+
+
+// decreaseItem function
+
+function decreaseItem(indivutual_item, products_object) {
+    indivutual_item.querySelector("#small_btn_minus").addEventListener("click", function() {
+        if (products_object.quantity > 1) {
+            indivutual_item.querySelector("#quantity").innerText = --products_object.quantity;
+
+        } else {
+            //remove in the cart and in the list
+            save_cart_items = save_cart_items.filter(newElemts => newElemts.id !== products_object.id)
+            indivutual_item.remove()
+            console.log(save_cart_items);
+
+        }
+
+        Calculate_Total()
+    })
+}
+
+
+
+
+// RemoveItem function
+
+
+function RemoveItem(indivutual_item, products_object) {
+    indivutual_item.querySelector("#small_btn_remove").addEventListener("click", function() {
+        console.log("Remove");
+        //Remove in the cart
+        indivutual_item.remove()
+
+        //Remove in array or list
+        save_cart_items = save_cart_items.filter(newElemts => newElemts.id !== products_object.id)
+
+        //add calculate fucntion
+        Calculate_Total()
+    })
+}
---
+++ script.js
@@ -0,0 +1,112 @@
+$("#newProduct").click(function(){
+    console.log('yes welcome');
+    $('#display-modal').modal("show")
+})
+
+
+// input image file
+let input_img=document.querySelector("#img");
+//display-image
+let display_img=document.getElementById("display-img")
+console.log(display_img);
+
+
+//event input-img
+input_img.addEventListener("change",()=>{
+    display_img.src=URL.createObjectURL(input_img.files[0])
+    console.log(display_img);
+})
+
+console.log(display_img.src);
+
+
+
+//select all inputs
+let image=$("#img");
+let P_name=$("#p-name");
+let Desc=$("#desc");
+let price=$("#price");
+
+
+
+//row div
+
+let rows_products=$("#rows_products");
+console.log(rows_products);
+
+function display_product(sawir,Pro_name,descrip,price){
+rows_products.append(`
+
+<div class="col-lg-4 col-md-6 col-sm-12 my-3 ">
+<!-- card product -->
+<div class="card p-2 shadow-lg" style="width: 25rem;" id=""card>
+  <img src="${sawir}" class="card-img-top" id="sawir"  style="height: 250px;">
+  <div class="card-body">
+    <h5 class="card-title">${Pro_name}</h5>
+    <div class="d-flex justify-content-between">
+      <p class="card-text">${descrip}</p>
+      <p class="card-text fs-4 text-primary">$${price}</p>
+     </div>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+</div>
+
+
+`)
+
+
+
+
+}
+
+
+
+
+
+$("#form-product").submit(function(event){
+    event.preventDefault();
+    display_product(display_img.src,P_name.val(),Desc.val(),price.val())
+   
+  
+
+    $("#form-product")[0].reset()
+
+
+   
+   
+   
+})
+
+
+//add localstorage
+
+
+//display on local
+
+
+
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#card div").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
